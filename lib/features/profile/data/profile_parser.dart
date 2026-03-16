d0c427d105202ad32f7033c36954ef2299e9d27d
MyCum <52251763+mycum@users.noreply.github.com>
2026-03-17 03:52:57 +0700

    Update profile parser to disable balancer
    Modify profile override to disable balancer strategy.

--- lib/features/profile/data/profile_parser.dart
+++ lib/features/profile/data/profile_parser.dart
@@ -378,7 +378,10 @@ class ProfileParser {
           (key, value) => !allowedOverrideConfigs.contains(key) || value == null || value.toString().isEmpty,
         );
 
-        final profileOverrideStr = jsonEncode({for (final key in headers.keys) key: headers[key]});
+        // Создаем жесткий оверрайд, отключающий балансировщик (заставляя использовать Lowest)
+        final overrideMap = {for (final key in headers.keys) key: headers[key]};
+        overrideMap['balancer-strategy'] = 'none'; // Отключаем балансировщик!
+        final profileOverrideStr = jsonEncode(overrideMap);
 
         return profile.map(
           remote: (rp) => rp.copyWith(
