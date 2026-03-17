25e5ee5f89d789205283ac4485b685debfa9a741
MyCum <52251763+mycum@users.noreply.github.com>
2026-03-17 07:02:53 +0700

    Refactor IpInfoNotifier and update imports

--- lib/features/proxy/active/active_proxy_notifier.dart
+++ lib/features/proxy/active/active_proxy_notifier.dart
@@ -6,70 +6,12 @@ import 'package:hiddify/core/preferences/general_preferences.dart';
 import 'package:hiddify/core/utils/throttler.dart';
 import 'package:hiddify/features/connection/notifier/connection_notifier.dart';
 import 'package:hiddify/features/proxy/data/proxy_data_providers.dart';
-import 'package:hiddify/features/proxy/model/ip_info_entity.dart' as oldipinfo;
-import 'package:hiddify/features/proxy/model/proxy_failure.dart';
-import 'package:hiddify/hiddifycore/generated/v2/hcore/hcore.pb.dart';
-import 'package:hiddify/hiddifycore/init_signal.dart';
-import 'package:hiddify/utils/riverpod_utils.dart';
-import 'package:hiddify/utils/utils.dart';
+import 'package:hiddify/features/proxy/model/ip_info_entity.dart';
+import 'package:hiddify/features/proxy/model/proxy_entity.dart';
+import 'package:hiddify/utils/custom_loggers.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'active_proxy_notifier.g.dart';
-
-@riverpod
-class IpInfoNotifier extends _$IpInfoNotifier with AppLogger {
-  @override
-  Future<oldipinfo.IpInfo> build() async {
-    ref.disposeDelay(const Duration(seconds: 20));
-    final cancelToken = CancelToken();
-    Timer? timer;
-    ref.onDispose(() {
-      loggy.debug("disposing");
-      cancelToken.cancel();
-      timer?.cancel();
-    });
-
-    ref.listen(serviceRunningProvider, (_, next) => _idle = false);
-
-    final autoCheck = ref.watch(Preferences.autoCheckIp);
-    final serviceRunning = await ref.watch(serviceRunningProvider.future);
-    // loggy.debug(
-    //   "idle? [$_idle], forced? [$_forceCheck], connected? [$serviceRunning]",
-    // );
-    if (!_forceCheck && !serviceRunning) {
-      throw const ServiceNotRunning();
-    } else if ((_idle && !_forceCheck) || (!_forceCheck && serviceRunning && !autoCheck)) {
-      throw const UnknownIp();
-    }
-
-    _forceCheck = false;
-    final info = await ref.watch(proxyRepositoryProvider).getCurrentIpInfo(cancelToken).getOrElse((err) {
-      loggy.warning("error getting proxy ip info", err, StackTrace.current);
-      // throw err; //hiddify: remove exception to be logged
-      throw const UnknownIp();
-    }).run();
-
-    timer = Timer(const Duration(seconds: 10), () {
-      loggy.debug("entering idle mode");
-      _idle = true;
-      ref.invalidateSelf();
-    });
-
-    return info;
-  }
-
-  bool _idle = false;
-  bool _forceCheck = false;
-
-  Future<void> refresh() async {
-    if (state.isLoading) return;
-    loggy.debug("refreshing");
-    state = const AsyncLoading();
-    await ref.read(hapticServiceProvider.notifier).lightImpact();
-    _forceCheck = true;
-    ref.invalidateSelf();
-  }
-}
 
 @riverpod
 class ActiveProxyNotifier extends _$ActiveProxyNotifier {
@@ -77,7 +19,7 @@ class ActiveProxyNotifier extends _$ActiveProxyNotifier {
   Stream<ProxyEntity?> build() async* {
     final stream = ref.watch(proxyRepositoryProvider).watchActiveProxy();
     await for (final proxy in stream) {
-      // ПЕРЕХВАТЧИК: Если ядро отдало балансер, принудительно переключаем на Lowest (auto)
+      // ---> ПЕРЕХВАТЧИК BALANCE <---
       if (proxy != null && proxy.tag == "balance") {
         ref.read(proxyRepositoryProvider).selectProxy("PROXY", "auto").run();
       }
@@ -86,18 +28,65 @@ class ActiveProxyNotifier extends _$ActiveProxyNotifier {
   }
 }
 
-  final _urlTestThrottler = Throttler(const Duration(seconds: 1));
+@riverpod
+class IpInfoNotifier extends _$IpInfoNotifier with AppLogger {
+  @override
+  Stream<IpInfo> build() async* {
+    final connected = await ref.watch(
+      connectionNotifierProvider.selectAsync((data) => data.isConnected),
+    );
+    if (!connected) return;
 
-  Future<void> urlTest(String? groupTag_) async {
-    final groupTag = groupTag_ ?? "";
-    _urlTestThrottler(() async {
-      if (state case AsyncData()) {
-        await ref.read(hapticServiceProvider.notifier).lightImpact();
-        await ref.read(proxyRepositoryProvider).urlTest(groupTag).getOrElse((err) {
-          loggy.warning("error testing group", err);
-          throw err;
-        }).run();
+    final proxyEither = await ref.watch(activeProxyNotifierProvider.future);
+    if (proxyEither == null) return;
+
+    yield* _ipInfoStream(proxyEither.tag).distinct();
+  }
+
+  bool _idle = false;
+  Timer? timer;
+
+  Stream<IpInfo> _ipInfoStream(String tag) async* {
+    final throttle = Throttler(const Duration(milliseconds: 100));
+    final dio = ref.watch(dioProvider);
+
+    while (true) {
+      if (_idle) {
+        loggy.debug("idle");
+        await Future.delayed(const Duration(seconds: 1));
+        continue;
       }
-    });
+      loggy.debug("fetching ip info");
+
+      try {
+        if (!ref.read(Preferences.connectionIpCheck)) {
+          loggy.debug("ip check is disabled");
+          yield const IpInfo.disabled();
+          _idle = true;
+          continue;
+        }
+        final response = await dio.get<Map<String, dynamic>>(
+          "http://cp.cloudflare.com",
+          options: Options(
+            receiveTimeout: const Duration(seconds: 5),
+            sendTimeout: const Duration(seconds: 5),
+          ),
+        );
+        yield IpInfo.fromJson(response.data!);
+      } on DioException catch (e) {
+        loggy.warning("error getting ip info");
+        loggy.debug(e.message);
+        yield IpInfo.error(e.message ?? "network error");
+      } catch (e) {
+        loggy.warning("error getting ip info", e);
+        yield const IpInfo.error("unknown error");
+      }
+
+      timer = Timer(const Duration(seconds: 10), () {
+        _idle = true;
+        loggy.debug("idling ip check");
+      });
+      throttle.call(() => _idle = false);
+    }
   }
 }
